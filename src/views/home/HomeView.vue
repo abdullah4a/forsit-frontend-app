c1894aa1bea9ccfe41cfbf41315ae32f4dae4db9
abdullah4a <abdullahkhan4a@gmail.com>
2023-09-29 22:39:06 +0500

    added chart

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,15 +1,40 @@
 <template>
   <AppData title="Home">
     <template #content>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam quidem ullam unde enim et. Eveniet blanditiis,
-      tenetur ea sapiente dignissimos sint corporis, similique modi, error possimus fuga. Expedita, quidem in.
+      <v-row>
+        <v-col>
+          <v-card height="500">
+          <v-card-title>chart</v-card-title>
+          <v-card-item>
+            <BarChart :points="chartData" :chartOption="chartOptions" />
+          </v-card-item>
+        </v-card>
+        </v-col>
+        <v-col>
+          <v-card height="500">
+          <v-card-title>chart</v-card-title>
+          <v-card-text>
+            <LineChart :points="chartData" :chartOption="chartOptions" />
+          </v-card-text>
+        </v-card>
+        </v-col>
+      </v-row>
     </template>
   </AppData>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { chartData, chartOptions } from '@/utils/chat-config';
 export default defineComponent({
   name: 'HomeView',
+  computed:{
+    chartData(){
+      return chartData
+    },
+    chartOptions(){
+      return chartOptions
+    }
+  }
 });
 </script>
