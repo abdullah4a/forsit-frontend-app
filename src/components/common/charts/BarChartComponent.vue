d0e4a6add9bae674e22be0825caf7684f4668ba6
abdullah4a <abdullahkhan4a@gmail.com>
2023-09-29 22:30:26 +0500

    made chart components

---
+++ src/components/common/charts/BarChartComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <Bar :data="chartData"  :options="options"/>
+</template>
+
+<script>
+import {Bar} from 'vue-chartjs'
+import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: {Bar},
+  props: {
+    points: Object,
+    chartOption: Object
+  },
+  computed: {
+    chartData(){
+      return this.points
+    },
+    options(){
+      return this.chartOption
+    }
+  }
+}
+</script>
---
+++ src/components/common/charts/PieChartComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <pie_chart :data="chartData" :options="options"/>
+</template>
+
+<script>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'vue-chartjs'
+ChartJS.register(ArcElement, Tooltip, Legend)
+export default {
+  name: 'BarChart',
+  components: {
+    pie_chart:Pie
+  },
+  props: {
+    points: Object,
+    chartOption: Object
+  },
+  computed: {
+    chartData(){
+      return this.points
+    },
+    options(){
+      return this.chartOption
+    }
+  }
+}
+</script>
