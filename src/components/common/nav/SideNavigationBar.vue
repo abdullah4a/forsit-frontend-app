25ed054e5b8d328f8b21949b84c0c261bb2e8020
abdullah4a <abdullahkhan4a@gmail.com>
2023-09-29 23:58:27 +0500

    fixed up formatting

--- src/components/common/nav/SideNavigationBar.vue
+++ src/components/common/nav/SideNavigationBar.vue
@@ -1,33 +1,26 @@
 <template>
-  <v-card>
-    <v-layout>
-      <v-navigation-drawer expand-on-hover rail>
-        <v-list>
-          <v-list-item :prepend-avatar="getUser.image" :title="getUser.name" :subtitle="getUser.email"></v-list-item>
-        </v-list>
-
-        <v-divider></v-divider>
-
-        <v-list density="compact" nav v-for="(menu, index) in menuTitles" :key="index">
-          <v-list-group :value="menu.name" v-if="menu.children.length > 0">
-            <template v-slot:activator="{ props }">
-              <v-list-item v-bind="props" title="Actions"></v-list-item>
-            </template>
-            <v-list-item v-for="({ name, icon, path }, i) in menu.children" :key="i" :value="name" :title="name"
-              :prepend-icon="icon" :to="{ name: path.name }"></v-list-item>
-          </v-list-group>
-          <v-list-item :prepend-icon="menu.icon" :title="menu.name" :value="menu.name" v-else
-            :to="{ name: menu.path.name }"></v-list-item>
-        </v-list>
-        <template v-slot:append>
-          <v-list>
-            <v-list-item prepend-icon="mdi-logout" title="Logout" :to="{name:'Home'}"></v-list-item>
-          </v-list>
+  <v-navigation-drawer expand-on-hover rail>
+    <v-list>
+      <v-list-item :prepend-avatar="getUser.image" :title="getUser.name" :subtitle="getUser.email"></v-list-item>
+    </v-list>
+    <v-divider></v-divider>
+    <v-list density="compact" nav v-for="(menu, index) in menuTitlesComp" :key="index">
+      <v-list-group :value="menu.name" v-if="menu.children.length > 0">
+        <template v-slot:activator="{ props }">
+          <v-list-item v-bind="props" title="Actions"></v-list-item>
         </template>
-      </v-navigation-drawer>
-      <slot name="main"></slot>
-    </v-layout>
-  </v-card>
+        <v-list-item v-for="({ name, icon, path }, i) in menu.children" :key="i" :value="name" :title="name"
+          :prepend-icon="icon" :to="{ name: path.name }"></v-list-item>
+      </v-list-group>
+      <v-list-item :prepend-icon="menu.icon" :title="menu.name" :value="menu.name" v-else
+        :to="{ name: menu.path.name }"></v-list-item>
+    </v-list>
+    <template v-slot:append>
+      <v-list>
+        <v-list-item prepend-icon="mdi-logout" title="Logout" :to="{ name: 'Home' }"></v-list-item>
+      </v-list>
+    </template>
+  </v-navigation-drawer>
 </template>
 
 <script lang="ts">
@@ -36,11 +29,9 @@ export default {
   computed: {
     getUser() {
       return { image: require("@/assets/logo.png"), name: "Admin", email: "admin@admin.com" }
-    }
-  },
-  data() {
-    return {
-      menuTitles: this.menuTitles
+    },
+    menuTitlesComp() {
+      return this.menuTitles
     }
   }
 }
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,18 +1,17 @@
 <template>
-    <SideNavigationBar>
-        <template #main>
-                <router-view></router-view>
-        </template>
-    </SideNavigationBar>
+    <v-app>
+        <SideNavigationBar/>
+        <slot name="main">
+            <router-view></router-view>
+        </slot>
+    </v-app>
 </template>
 
 <script lang="ts">
 import menuTitles from '@/utils/enum/MenuTitle.enum';
 export default {
-provide:{
-    menuTitles
-}
+    provide: {
+        menuTitles
+    }
 }
 </script>
-
-<style lang="scss" scoped></style>
