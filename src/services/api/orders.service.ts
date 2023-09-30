d6fd2eff4a068545b3a939c7a6be642005aa38d4
abdullah4a <abdullahkhan4a@gmail.com>
2023-09-30 18:30:30 +0500

    Added Services for Revenue and Sales and orders

---
+++ src/services/api/orders.service.ts
@@ -0,0 +1,35 @@
+// import API from './handlers/base.service';
+import { orders } from '../json/orders'
+const OrdersAPI = {
+
+    getOrderList(params) {
+        return new Promise<Array<Record<string, any>>>((resolve) => resolve(orders));
+    },
+    getTotalNumOfOrders() {
+        return new Promise<number>((resolve) => resolve(orders.length));
+    },
+    placeOrder(payload: any) {
+        orders.push(payload)
+        return new Promise<Record<string, any>>((resolve) => resolve(payload));
+    },
+    updateOrder(id: number, payload: any) {
+        const index=orders.findIndex(order=>order.id===id)
+        if (index>0) {
+            orders[index]={...payload}
+            return new Promise<Record<string, any>>((resolve) => resolve(payload));
+        }
+        else
+        return new Promise<Record<string, any>>((resolve, reject) => reject("Not Found"))
+    },
+    withdrawOrder(id: number) {
+        const index=orders.findIndex(order=>order.id===id)
+        if (index>0) {
+            orders.splice(index, 1)
+            return new Promise<Boolean>((resolve) => resolve(true));
+        }
+        else
+        return new Promise<Record<string, any>>((resolve, reject) => reject("Not Found"))
+    },
+};
+
+export default OrdersAPI;
---
+++ src/services/api/revenue.service.ts
@@ -0,0 +1,35 @@
+// import API from './handlers/base.service';
+import { revenues } from '../json/revenue'
+const RevenueAPI = {
+
+    getRevenueList() {
+        return new Promise<Array<Record<string, any>>>((resolve) => resolve(revenues));
+    },
+    getTotalNumOfRevenue() {
+        return new Promise<number>((resolve) => resolve(revenues.length));
+    },
+    createRevenue(payload: any) {
+        revenues.push(payload)
+        return new Promise<Record<string, any>>((resolve) => resolve(payload));
+    },
+    updateRevenue(id: number, payload: any) {
+        const revIndex=revenues.findIndex(rev=>rev.id===id)
+        if (revIndex>0) {
+            revenues[revIndex]={...payload}
+            return new Promise<Record<string, any>>((resolve) => resolve(payload));
+        }
+        else
+        return new Promise<Record<string, any>>((resolve, reject) => reject("Not Found"))
+    },
+    deleteRevenue(id: number) {
+        const revIndex=revenues.findIndex(rev=>rev.id===id)
+        if (revIndex>0) {
+            revenues.splice(revIndex, 1)
+            return new Promise<Boolean>((resolve) => resolve(true));
+        }
+        else
+        return new Promise<Record<string, any>>((resolve, reject) => reject("Not Found"))
+    },
+};
+
+export default RevenueAPI;
