42fd3848160e0166d0235170be23fd9bac098e59
abdullah4a <abdullahkhan4a@gmail.com>
2023-09-30 18:29:37 +0500

    Added Temp Json

---
+++ src/services/json/orders.ts
@@ -0,0 +1,20 @@
+export const orders = [
+  {
+    "id": 1,
+    "orderNumber": "ORD001",
+    "orderDate": "2023-09-15",
+    "status": "Shipped"
+  },
+  {
+    "id": 2,
+    "orderNumber": "ORD002",
+    "orderDate": "2023-09-20",
+    "status": "Processing"
+  },
+  {
+    "id": 3,
+    "orderNumber": "ORD003",
+    "orderDate": "2023-09-25",
+    "status": "Delivered"
+  }
+]
---
+++ src/services/json/sales.ts
@@ -0,0 +1,29 @@
+export const sales = [
+  {
+    "id":1,
+    "orderNumber": "ORD001",
+    "customerName": "John Doe",
+    "product": "Product A",
+    "quantity": 5,
+    "unitPrice": 50,
+    "totalAmount": 250
+  },
+  {
+    "id":2,
+    "orderNumber": "ORD002",
+    "customerName": "Jane Smith",
+    "product": "Product B",
+    "quantity": 3,
+    "unitPrice": 40,
+    "totalAmount": 120
+  },
+  {
+    "id":3,
+    "orderNumber": "ORD003",
+    "customerName": "Alice Johnson",
+    "product": "Product C",
+    "quantity": 2,
+    "unitPrice": 30,
+    "totalAmount": 60
+  }
+]
